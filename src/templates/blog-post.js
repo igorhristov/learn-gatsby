0d6ed195202374c8476b2882a1edae55e9983ec8
igorhristov <igorhristov@sourcemxacademy.mk>
2020-09-23 22:38:45 +0200

    Programmatically Create pages
    Creating Slugs with Node
    Creating Pages with Node

---
+++ src/templates/blog-post.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Layout from '../components/layout2';
+
+export default () => {
+  return (
+    <Layout>
+      <div>hello blog post</div>
+    </Layout>
+  );
+};
